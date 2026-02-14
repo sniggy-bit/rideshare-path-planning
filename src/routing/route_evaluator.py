39893ea2f7e741eaab722e7e723182f2db6b6837
Snigdha Prakash <snigdhaprakash4@gmail.com>
2026-02-13 18:21:12 -0800

    finished path_cost_finder

--- src/routing/route_evaluator.py
+++ src/routing/route_evaluator.py
@@ -5,5 +5,17 @@ import numpy as np
 from src.graph.bfs import bfs_shortest_path
 from src.graph.grid import Grid
 
-def path_cost_finder(grid, stops, bfs_shortest_path):
-    return 
+#Path cost finder function: given a grid, a valid route and a bfs_shortest_path function, 
+# returns the total cost of the path that goes through all the stops in the order they are given
+def path_cost_finder(grid, route, bfs_shortest_path):
+    total_cost = 0
+    for i in range(len(route)-1):
+        start = route[i][2]
+        end = route[i+1][2]
+        #find shortest path between two adjacent stops using bfs_shortest_path function
+        path = bfs_shortest_path(grid, start, end)
+        print(f"Path from {start} to {end}: {path}")
+        if path["length"] is None:
+            return None #If there is no path between two stops, return infinity
+        total_cost += path["length"] #The cost of the path is the number of edges, which is the number of nodes - 1
+    return total_cost
