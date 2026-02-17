d3b15ea93a2bdf1465c9b1403934cc6bb7bd8976
Snigdha Prakash <snigdhaprakash4@gmail.com>
2026-02-16 19:32:18 -0800

    optimized w/ path cache

--- src/routing/route_evaluator.py
+++ src/routing/route_evaluator.py
@@ -7,15 +7,26 @@ from src.graph.grid import Grid
 
 #Path cost finder function: given a grid, a valid route and a bfs_shortest_path function, 
 # returns the total cost of the path that goes through all the stops in the order they are given
-def path_cost_finder(grid, route, bfs_shortest_path):
+def path_cost_finder(grid, route, bfs_shortest_path,cache):
+
     total_cost = 0
     for i in range(len(route)-1):
         start = route[i][2]
         end = route[i+1][2]
-        #find shortest path between two adjacent stops using bfs_shortest_path function
-        path = bfs_shortest_path(grid, start, end)
-        print(f"Path from {start} to {end}: {path}")
-        if path["length"] is None:
-            return None #If there is no path between two stops, return infinity
-        total_cost += path["length"] #The cost of the path is the number of edges, which is the number of nodes - 1
+        #Create a key for the memoization dictionary
+        pair_key = tuple(sorted((start, end)))
+        
+        #Check if the cost for this pair of stops has already been computed
+        if pair_key in cache:
+            path_cost = cache[pair_key]
+        else:
+            path = bfs_shortest_path(grid, start, end)
+            if path is None:
+                #If there is no path between the two stops, return infinity
+                return float('inf') 
+            path_cost = path["length"]
+            #Store the computed cost in the memoization dictionary
+            cache[pair_key] = path_cost 
+        #The cost of the path is the number of edges, which is the number of nodes - 1
+        total_cost += path_cost
     return total_cost
