165ff20310ea409444ccc8b8c66b9107af795020
Snigdha Prakash <snigdhaprakash4@gmail.com>
2026-02-27 18:55:09 -0800

    added heuristic function wip

--- src/routing/static_planner.py
+++ src/routing/static_planner.py
@@ -6,12 +6,13 @@
 import numpy as np
 from itertools import permutations
 from src.routing import events
-from src.graph.bfs import bfs_shortest_path
+from src.graph.path_finder import bfs_shortest_path
 from src.graph.grid import Grid 
 from typing import List, Tuple
 
-#Static planner function: takes in a grid map, a set of passenger requests, and a vehicle's current location. Returns an optimal path for the vehicle to pick up and drop off all passengers.
-def static_planner(grid_map, requests: events.RequestSet):
+#Route generator function: takes in a grid map, a set of passenger requests, and a vehicle's current location. 
+# Returns an optimal path for the vehicle to pick up and drop off all passengers.
+def route_generator(grid_map, requests: events.RequestSet):
     #TODO: Add grid map representation
 
     #TODO: Enumerate all valid pickup and dropoff sequences for the given requests
@@ -24,24 +25,18 @@ def static_planner(grid_map, requests: events.RequestSet):
     for request in requests.get_all_requests().values():
         stops.append((request.passenger_id,'pickup', request.pickup_location))
         stops.append((request.passenger_id,'dropoff', request.dropoff_location))
-    
-    print(f"Stops: {stops}")
-    print(f"Number of stops: {len(stops)}")
-    
+
     #Generate all valid sequences of stops (pickup and dropoff)
     
     sequences = list(permutations(stops))
-    print(f"Total sequences: {len(sequences)}")
-    print(f"Sample sequence: {sequences[0]}")
     valid_sequences = []
     
     for seq in sequences:
         if is_valid_sequence(seq):
             valid_sequences.append(seq)
-    #print(f"Valid sequences: {valid_sequences}")
-    print(f"Number of valid sequences: {len(valid_sequences)}")
     return valid_sequences
 
+#Helper function to check if a sequence of stops is valid
 def is_valid_sequence(sequence):
         
         if (sequence[0][0] != 'A') or (sequence[0][1] != 'pickup'):
@@ -56,5 +51,6 @@ def is_valid_sequence(sequence):
                     return False 
         return True
 
-
+def calculate_heuristic(start, end, users_in_car, alpha):
+     return get_dist(start, end) + alpha * len(users_in_car)
     
