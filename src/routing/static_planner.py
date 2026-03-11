3ff919f771c35d1038d0aca421070cd9655f9269
Snigdha Prakash <snigdhaprakash4@gmail.com>
2026-03-11 15:29:27 -0700

    implemented generate_valid_routes w/o cost updation

--- src/routing/static_planner.py
+++ src/routing/static_planner.py
@@ -59,62 +59,69 @@ def route_generator(grid: Grid, requests: events.RequestSet, gamma = 1.5):
     #Initialize state of taxi when the ride starts, pick up A
     start = request_dict[passenger_ids[0]]
     route = [(passenger_ids[0], "pickup", start.pickup_location)]
-    initial_state = TaxiState(location = start.pickup_location, waiting=passenger_ids, in_car=())
+    #node representation: (location, waiting passengers, passengers in car, total T = 0 and total Q = 0)
+    initial_state = TaxiState(location=start.pickup_location, 
+                              waiting=passenger_ids, 
+                              in_car=(start[0]), 
+                              total_t=0, 
+                              total_q=0, 
+                              time=0, 
+                              route=route)
     
     #A* SEARCH 
+    
+    #frontier -> priority queue ordered by total J = T + Q + h(route) + h(quality))
     open_set = [initial_state]
+    #min heap the frontier to ensure the state with lowest total J is always popped first
     heap = heapq.heapify(open_set)
     
-    #Tracks visited states to avoid cycling
+    #explored -> Tracks visited states to avoid cycling 
+    # links state to total cost to get there.
+    #in case a more efficient path to prev visited stated is found, the state can be added back to
+    #frontier with updated total J in constant time
     visited = {(initial_state.location, initial_state.waiting, initial_state.in_car): initial_state.total_j}
 
     #Loop to search for optimal route
     while open_set:
         current_state = heapq.heappop(open_set)
-
-        #Check whether the current state is the goal state (all passengers dropped)
+       
+       #goal-test: Check whether the current state is the goal state (all passengers dropped)
         if current_state.waiting == () and current_state.in_car == ():
             return current_state.route
-    
-        #Ensure that cycling is avoided by checking whether this is the best path to this state so far
-        state_id = (current_state.location, current_state.waiting, current_state.in_car)
-        if state_id not in visited or visited[state_id] < current_state.total_j:
-            #Update current total J to be the lowest for this state
-            visited[state_id] = current_state.total_j
-            for next_state in valid_next_states(current_state, distance_cache, gamma):
+        
+        #add curent state to explored (closed) set:
+        current_state_id = (current_state.location, current_state.waiting, current_state.in_car)
+        visited[current_state_id] = current_state.total_j
+       
+        #******ADD HEURISTIC CALCULATION LATER******#
+        for next_state in generate_next_states(current_state, request_dict, distance_cache, gamma):
+            #Ensure that cycling is avoided by checking whether this is the best path to this state so far
+            state_id = (next_state.location, next_state.waiting, next_state.in_car)
+            if state_id not in open_set or visited:
+            #Update current state in frontier containing total J value
                 heapq.heappush(open_set, next_state)
-    return
+            elif next_state.total_j < visited[state_id]:
+                heapq.heappush(open_set, next_state)
+                #update cost to the new best cost for this state in the visited set
+                visited[state_id] = next_state.total_j    
+            #check order of the above later
+    #if frontier is empty and goal state doesn't exist, return failure
+    return None
 
-
-#Helper function to check if an action is valid (e.g. no dropping off before pickup, A must be picked up first, etc.)
-def is_valid_action(sequence):
-    #Ensure A is always picked up first
-    if (sequence[0][0] != 'A') or (sequence[0][1] != 'pickup'):
-            return False
-    picked_up = set()
-    #Iterate through route so far to ensure no passenger is dropped off before being picked up
-    for stop in sequence:
-        (passenger_id, action, location) = stop
-        if action == 'pickup':
-            picked_up.add(passenger_id)
-        elif action == 'dropoff':
-            if passenger_id not in picked_up:
-                return False 
-    return True
 #Helper function to generate valid next states from the current state,
 #based on the distance cache and gamma weighting factor for user quality
 
-#This is basically similar to get neighbors in a bfs/djikstra
+#Similar to get neighbors in a bfs/djikstra
 #For each waiting passenger, we can generate a state where they are picked up
 #For each passenger in the car, we generate a state where they are dropped off
 
-def valid_next_states(current_state, request_dict, distance_cache, gamma):
+#generates the child nodes for current state
+def generate_next_states(current_state, request_dict, distance_cache, gamma):
     next_states = []
     #Generate states for picking up or dropping off each passenger
     for passenger_id in current_state.waiting:
-        next_states.append(request_dict[passenger_id].pickup_location, passenger_id, "pickup")
+        next_states.append((request_dict[passenger_id].pickup_location, passenger_id, "pickup"))
     for passenger_id in current_state.in_car:
-        next_states.append(request_dict[passenger_id].dropoff_location, passenger_id, "dropoff")
+        next_states.append((request_dict[passenger_id].dropoff_location, passenger_id, "dropoff"))
     return next_states
-    
     
